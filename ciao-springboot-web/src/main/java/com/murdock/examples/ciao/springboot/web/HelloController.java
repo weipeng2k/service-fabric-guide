e8c15868a4f5fa0a1f3088a96014e10eae98faa7
weipeng2k <weipeng2k@126.com>
2018-03-25 21:09:18 +0800

    fix(ciao-springboot-web): update pom

--- ciao-springboot-web/src/main/java/com/murdock/examples/ciao/springboot/web/HelloController.java
+++ ciao-springboot-web/src/main/java/com/murdock/examples/ciao/springboot/web/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -18,7 +21,14 @@ public class HelloController {
     public String hello(Model model) {
         try {
             InetAddress localHost = InetAddress.getLocalHost();
+
+            RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+            long startTime = runtimeMXBean.getStartTime();
+            long uptime = runtimeMXBean.getUptime();
+
             model.addAttribute("ip", localHost.getHostAddress());
+            model.addAttribute("startTime", startTime);
+            model.addAttribute("uptime", uptime);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
